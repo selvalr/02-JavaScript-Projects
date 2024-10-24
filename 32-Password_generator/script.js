60669bc9ae31ddfee63f8a9a7b9e77174a4cb0bc
Selva_lr <84980271+selvalr@users.noreply.github.com>
2024-10-24 08:40:10 +0000

    Password Generater

---
+++ 32-Password_generator/script.js
@@ -0,0 +1,25 @@
+function randomPassword() {
+  let displayPassword = document.getElementById("display-password");
+  let passwordLength = document.getElementById("password-length").value;
+  let error = document.getElementById("error-info");
+  let password = "";
+  let characters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+  parseInt(passwordLength);
+
+  if (passwordLength == 0) {
+    error.innerHTML = "Please select password length";
+    displayPassword.value = "Please select password length";
+    error.style.padding = "10px 15px";
+    setTimeout(() => {
+      error.style.visibility = "hidden";
+      window.location.reload();
+    }, 1000);
+  }
+
+  for (let i = 0; i < passwordLength; i++) {
+    password += characters[Math.floor(Math.random() * characters.length)];
+  }
+  displayPassword.value = password;
+}
