86ced1e2a9115b2a630fef7c04a948329e07036a
Selva_lr <84980271+selvalr@users.noreply.github.com>
2025-03-11 10:57:01 +0000

    Form Wave

---
+++ 74-Form-Wave/script.js
@@ -0,0 +1,12 @@
+const allItem=document.querySelectorAll(".form-control label");
+
+allItem.forEach((label)=>{
+   label.innerHTML=label.innerHTML.split("")
+   .map((letter,index)=>
+     `<span style="transition-delay:${index * 50}ms">${letter}</span>`
+//     console.log(letter,index);
+    
+   )   
+   .join("");
+})
+
